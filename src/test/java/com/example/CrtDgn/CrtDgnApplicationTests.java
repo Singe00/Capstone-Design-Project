5b15c128f166f409c9b6965f650c1c70acaa1d0f
Singe00 <singery00@gmail.com>
2023-05-02 22:33:13 +0900

    검색 기능

--- src/test/java/com/example/CrtDgn/CrtDgnApplicationTests.java
+++ src/test/java/com/example/CrtDgn/CrtDgnApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.CrtDgn;
 
 import com.example.CrtDgn.Login.Dto.MemberDto;
 import com.example.CrtDgn.Login.Service.MemberService;
+import com.example.CrtDgn.Search.Controller.SearchController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,9 @@ class CrtDgnApplicationTests {
 
 	@Autowired
 	private MemberService memberService;
+
+	@Autowired
+	private SearchController searchController;
 
 
 	@Test
@@ -24,5 +28,9 @@ class CrtDgnApplicationTests {
 		memberService.signup(request1);
 	}
 
+	@Test
+	void TourSearch(){
+		searchController.searchToursByTitle("숲");
+	}
 
 }
